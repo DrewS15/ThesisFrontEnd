3c5ea115f5bce11950e8d813c7b34bde40fd17c3
DrewS15 <andsanjuan@gbox.adnu.edu.ph>
2023-05-08 12:04:42 +0800

    Created New Branch for final changes

--- popup.js
+++ popup.js
@@ -4,30 +4,6 @@ chrome.storage.local.get('highlighted_text', function(result){
   document.getElementById('text_display').innerHTML = selected;
 });
 
-// const BASE_URL = 'https://cf98-124-106-181-212.ngrok-free.app';
-// const sentiment = 'tangina pareh dami mga shawty sa bgc pareh';
-// const sentiment = 'gusto ko na mamatay';
-// const sentiment = text_display.value;
-// const queryParam = `/?input='${sentiment}'`;
-// console.log(selected)
-
-
-// const path = `${BASE_URL}${queryParam}`
-// const path = JSON.parse(`${BASE_URL}${queryParam}`)
-// fetch(`${BASE_URL}${queryParam}`)
-//   .then(response => response.text())
-//   .then(data => console.log(data));
-// console.log(`${BASE_URL}${queryParam}`)
-
-// fetch(`${BASE_URL}${queryParam}`, {
-//   method: 'GET',
-//   headers: {
-//     'ngrok-skip-browser-warning': '1234'
-//   }
-// })
-//       .then(response => response.json())
-//       .then(data => console.log(data.Result));
-
 const analyzeBtn = document.getElementById("analyze");
 const decisionDisplay = document.getElementById("decision_display");
 
