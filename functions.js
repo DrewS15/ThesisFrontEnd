d068141ad01971cd61e79b50630855b4fa4339c4
DrewS15 <andsanjuan@gbox.adnu.edu.ph>
2023-05-10 20:40:01 +0800

    Changes 6.0.3

--- functions.js
+++ functions.js
@@ -1,3 +1,4 @@
 var manifestData = chrome.runtime.getManifest();
 document.getElementById("version").innerHTML = " Version Alpha " + manifestData.version;
 document.getElementById("title").innerHTML = manifestData.name;
+
