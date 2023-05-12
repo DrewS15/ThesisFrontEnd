6bc8495ef0a91fb2fa632018ad0ff105e01439f1
DrewS15 <andsanjuan@gbox.adnu.edu.ph>
2023-05-12 18:00:05 +0800

    Changes 8.0 Added Notification upon selecting custom Context Menu Item Added temporary icon

--- cloudService.js
+++ cloudService.js
@@ -5,14 +5,10 @@ chrome.storage.local.get('highlighted_text', function(result){
 
 const analyzeBtn = document.getElementById("analyze");
 const decisionDisplay = document.getElementById("decision_display");
+const decisionLabel = document.getElementById("decision_label");
 var isClick = true;
 
 analyzeBtn.addEventListener("click", function() {
-  preprocess();
-});
-
-function preprocess(){
-  console.log("Preprocessing");
   var sentiment =  document.getElementById('input_text').value;
   console.log("Value: " + sentiment);
   if(sentiment==""){
@@ -22,7 +18,7 @@ function preprocess(){
   else{
     if(isClick){
       console.log("Input string: " + sentiment);
-      document.getElementById('analyze').innerHTML = "Clear Input";
+      document.getElementById('analyze').innerHTML = "Clear";
       document.getElementById("input_text").readOnly = true;
       // !!!need to change BASE_URL everytime there is a new session
       // !!!get url from val
@@ -36,21 +32,27 @@ function preprocess(){
         }
       })
             .then(response => response.json())
-            .then(data => decisionDisplay.value = data.Result)
+            .then(data => decisionDisplay.innerHTML = data.Result);
       isClick = false;
+      decisionDisplay.style.display = "block";
+      decisionLabel.style.display = "block";
     }
     else{
       console.log("Clear!");
       chrome.storage.local.set({ highlighted_text : null});
       document.getElementById("input_text").readOnly = false;
-      document.getElementById("analyze").innerHTML = "Analyze Text";
+      document.getElementById("analyze").innerHTML = "Analyze";
       document.getElementById("input_text").value = null;
-      document.getElementById("decision_display").value = null;
+      document.getElementById("decision_display").innerHTML = null;
       sentiment = null;
       isClick = true;
+      decisionDisplay.style.display = "none";
+      decisionLabel.style.display = "none";
     }
   }
-}
+});
+
+
 
 
 
--- contextMenu.js
+++ contextMenu.js
@@ -1,5 +1,6 @@
 chrome.storage.local.set({ highlighted_text : null});
 chrome.runtime.onInstalled.addListener(() => {
+    notifyUser();
     // context menu information for chrome extension
     // when user right clicks on a text, the extension be available in the options
     var contextMenuItem = {
@@ -14,12 +15,28 @@ chrome.runtime.onInstalled.addListener(() => {
     // when user clicks on the extension, the selected text will be stored in the local storage
     chrome.contextMenus.onClicked.addListener(function(info, tab){
       //store the selected text in variable selectedText
-      var selectedText = info.selectionText
+        var selectedText = info.selectionText
           //store the selected text in local storage
-          chrome.storage.local.set({ highlighted_text : selectedText})
+        chrome.storage.local.set({ highlighted_text : selectedText})
+        notifyUser();
 
     });
 });
+
+function notifyUser(){
+    console.log("Create Notification");
+    chrome.notifications.create(
+        {
+            
+            title: 'Filipino Hatespeech Detection',
+            message: 'Selection saved, Open extension',
+            iconUrl: 'images/32x32_apple_texture.png',
+            type: 'basic'
+        }
+    );
+}
+
+
 
 
 
